e762d36287f23f9d1a5a9cd550e23ca7a748c30c
Benz Peerakit <peerakitzaa001@hotmail.com>
2024-08-08 22:48:14 +0700

    Add data

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
 import './App.css';
 import { Header, AddForm, Item } from './components';
 import { Task } from './interface/interface';
@@ -7,19 +7,34 @@ function App() {
   const [tasks, setTasks] = useState<Task[]>([
     { id: 1, title: 'แก้บัคโปรแกรม' },
     { id: 2, title: 'คู่มือใช้โปรแกรม' },
-    { id: 3, title: 'กดเงินที่ธนาคาร' },
   ]);
+
+  const [title, setTitle] = useState<string>('');
 
   const deleteTask = (id: number) => {
     const result = tasks.filter((vaule) => vaule.id != id);
     setTasks(result);
   };
 
+  const saveTask = (e: FormEvent) => {
+    e.preventDefault();
+    if (!title) {
+      alert('กรุณาป้อนข้อมูลด้วยครับ');
+    } else {
+      const newTask = {
+        id: Math.floor(Math.random() * 1000),
+        title: title,
+      };
+      setTasks([...tasks, newTask]);
+      setTitle('');
+    }
+  };
+
   return (
     <div className="App">
       <Header />
       <div className="container">
-        <AddForm />
+        <AddForm title={title} setTitle={setTitle} saveTask={saveTask} />
         <section>
           {tasks.map((data) => (
             <Item key={data.id} data={data} deltask={deleteTask} />
--- src/components/AddForm/index.tsx
+++ src/components/AddForm/index.tsx
@@ -1,12 +1,25 @@
-import "./AddForm.css";
+import { FormEvent } from 'react';
+import './AddForm.css';
 
-export default function AddForm() {
+interface AddFormProps {
+  title: string;
+  setTitle: (title: string) => void;
+  saveTask: (e: FormEvent) => void;
+}
+
+export default function AddForm(props: AddFormProps) {
+  const { title, setTitle, saveTask } = props;
   return (
     <>
       <h2>แอพบริหารจัดการงาน</h2>
-      <form>
+      <form onSubmit={saveTask}>
         <div className="form-control">
-          <input type="text" className="text-input" />
+          <input
+            type="text"
+            className="text-input"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
           <button type="submit" className="submit-btn">
             เพิ่ม
           </button>
